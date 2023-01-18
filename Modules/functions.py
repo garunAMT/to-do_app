86854623d46a98d1844096f9544af756d2b68ee4
garunAMT <21ME02022@iitbbs.ac.in>
2023-01-18 16:16:52 +0530

    added edit and add todo features.

--- Modules/functions.py
+++ Modules/functions.py
@@ -4,6 +4,6 @@ def get_todos(filepath='todos.txt'):
     return todos_local
 
 
-def write_todos(filepath, todos_arg):
+def write_todos(todos_arg, filepath='todos.txt'):
     with open(filepath, 'w') as file:
         file.writelines(todos_arg)
